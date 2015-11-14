ccef4b59bae8422705398e90cb0f5e1460a7b82f
zhengsonglan <1300326388@qq.com>
2015-11-14 16:12:11 +0800

    有bug动态计算高度

--- app/src/main/java/com/zsl/customviewdemo/view/CustomTitleView.java
+++ app/src/main/java/com/zsl/customviewdemo/view/CustomTitleView.java
@@ -33,11 +33,11 @@ public class CustomTitleView extends View {
 
 
     public CustomTitleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CustomTitleView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public CustomTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -75,9 +75,41 @@ public class CustomTitleView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize=MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
+
+        int width,height;
+        if (widthMode==MeasureSpec.EXACTLY){
+            width=widthSize;
+        }else{
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText,0,mTitleText.length(),mBound);
+            float textWidth=mBound.width();
+            int desired= (int) (getPaddingLeft()+textWidth+getPaddingRight());
+            width=desired;
+        }
+
+
+        if(heightMode==MeasureSpec.EXACTLY){
+            height=heightSize;
+        }else{
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText,0,mTitleText.length(),mBound);
+            float textHeight=mBound.height();
+
+            int desired= (int) (getPaddingTop()+textHeight+getPaddingBottom());
+            height=desired;
+        }
+        setMeasuredDimension(width,height);
     }
 
+    /**
+     * 绘制方法
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
         //绘制一个矩形颜色为黄色
